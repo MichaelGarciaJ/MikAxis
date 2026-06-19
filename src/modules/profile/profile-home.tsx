dbb16bf36ed47ddc1831f6ca4d1c8aa43ff35e25
MichaelGarciaJ <mgarcia271476@gmail.com>
2026-06-19 23:06:26 +0200

    Corrección de formato de avatares a JPEG

--- src/modules/profile/profile-home.tsx
+++ src/modules/profile/profile-home.tsx
@@ -39,7 +39,9 @@ export default function ProfileHome() {
     '/avatars/avatar2.png', 
     '/avatars/avatar3.png', 
     '/avatars/avatar4.png', 
-    '/avatars/avatar5.png'
+    '/avatars/avatar5.png',
+    '/avatars/avatar6.jpg',
+    '/avatars/avatar7.jpg'
   ];
   
   const navigate = useNavigate();
